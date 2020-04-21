f57df6aad5da43059e5bf539d0e730bfb78510bd
varungarg2796 <varungarg2796@hotmail.com>
2020-04-21 23:14:25 +0100

    adding singup form and semantic.css for styling

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  authForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirmation: new FormControl('')
+  });
   constructor() { }
 
   ngOnInit() {
