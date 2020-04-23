8f8a497141145dc26f50fbc66a2be5d7123c0de4
varungarg2796 <varungarg2796@hotmail.com>
2020-04-23 01:30:28 +0100

    using async pipe for behaviour subject to main auth state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  signedin = false;
-  constructor(private authService: AuthService) {}
-  title = 'emailApp';
-
-  ngOnInit() {
-    this.authService.signedin$.subscribe( (signedin) => {
-      this.signedin = signedin;
-    });
+export class AppComponent {
+  signedin$: BehaviorSubject<boolean>;
+  constructor(private authService: AuthService) {
+    this.signedin$ = this.authService.signedin$;
   }
 }
