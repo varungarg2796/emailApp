69f2e6b761927a2499868123841d3e906bba6778
varungarg2796 <varungarg2796@hotmail.com>
2020-04-25 23:59:00 +0100

    fixing the guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,20 +2,25 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Route, RouterStateSnapshot, UrlSegment, UrlTree, CanLoad, Router } from '@angular/router';
 import { Observable, Subscriber } from 'rxjs';
 import { AuthService } from './auth.service';
-import { take, skipWhile } from 'rxjs/operators';
+import { take, skipWhile, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad  {
 
-  constructor( private authService: AuthService) {}
+  constructor( private authService: AuthService, private router: Router) {}
   canLoad(route: Route, segments: UrlSegment[]): Observable<boolean>|Promise<boolean>|boolean {
     // return false; // false for not allowing, true for allowing
 
     return this.authService.signedin$.pipe(
       skipWhile( value => value === null), // I don't care about values which is null
-      take(1) // taking one value which satisfies skipWhile
+      take(1), // taking one value which satisfies skipWhile
+      tap( (isSignedIn) => {
+        if (!isSignedIn) {
+          this.router.navigateByUrl('/');
+        }
+      })
     );
   }
 }
