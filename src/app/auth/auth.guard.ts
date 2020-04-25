442fb0f885e154b301c8fce09f8ae485604b9791
varungarg2796 <varungarg2796@hotmail.com>
2020-04-25 23:38:18 +0100

    Add guard to protect routes

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Route, RouterStateSnapshot, UrlSegment, UrlTree, CanLoad } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad  {
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean>|Promise<boolean>|boolean {
+    return false; // false for not allowing, true for allowing
+  }
+}
+
