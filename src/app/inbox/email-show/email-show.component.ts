2111f7e3094d996de4b73ac4c005f87b1df4dd12
varungarg2796 <varungarg2796@hotmail.com>
2020-05-03 21:34:04 +0100

    using switchmap to cancel request using rxjs

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EmailService } from '../email.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-email-show',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailShowComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private emailService: EmailService) { }
 
   ngOnInit() {
+    // this.route.params.subscribe(({id}) => {
+    //   this.emailService.getEmail(id).subscribe(email => {
+    //     console.log(email);
+    //   });
+    // });
+
+    this.route.params.pipe(
+      switchMap( ({id}) => {
+        return this.emailService.getEmail(id);
+      })
+    ).subscribe( (email) => {
+      console.log(email);
+    });
   }
 
 }
