eb158756ceb79bc6fbb3c45febc03c8ffe9554aa
varungarg2796 <varungarg2796@hotmail.com>
2020-05-03 23:40:26 +0100

    error handling in resolver using catcherror and EMPTY.

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Email } from './email';
 import { EmailService } from './email.service';
+import { catchError } from 'rxjs/operators';
+import { of, EMPTY } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailResolverService implements Resolve<Email> {
 
-  constructor(private emailService: EmailService) { }
+  constructor(private emailService: EmailService, private router: Router) { }
   resolve(route: ActivatedRouteSnapshot) {
     const { id } = route.params;
-    return this.emailService.getEmail(id);
+    return this.emailService.getEmail(id).pipe(
+      catchError(() => {
+        this.router.navigateByUrl('/inbox/not-found');
+
+        return EMPTY; // observable which is marked as complete and we don't care about
+      })
+    );
   }
 }
