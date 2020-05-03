5770c8fd86157b9b766abb64c7cfd3b0d2f41660
varungarg2796 <varungarg2796@hotmail.com>
2020-05-03 23:58:26 +0100

    adding a modal for creating and replying to emails using elementRef, output, and event emitter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthHttpInterceptor } from './auth/auth-http-interceptor'
+import { AuthHttpInterceptor } from './auth/auth-http-interceptor';
 import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ],
   imports: [
     AuthModule,
     BrowserModule,
