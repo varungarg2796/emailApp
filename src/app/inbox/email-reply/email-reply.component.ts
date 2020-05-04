1226bdd3a64e73740feed2024e281469a52b2638
varungarg2796 <varungarg2796@hotmail.com>
2020-05-04 01:39:22 +0100

    replying to the emails

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-email-reply',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailReplyComponent implements OnInit {
 
+  showModal = false;
+  @Input() email: Email;
+
   constructor() { }
 
+
   ngOnInit() {
+    const text = this.email.text.replace(/\n/gi, '\n> '); // replace every new line in text with >
+    this.email = {  // WOW! Really neat way to swap fields!
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: 'RE' + this.email.subject,
+      text: `\n\n\n-------${this.email.from} wrote:\n>${text}`
+    };
+  }
+
+  onSubmit(email: Email) {
+
   }
 
 }
