0490fe0462c0ab7bed7ba658fb0e911925ec3d30
varungarg2796 <varungarg2796@hotmail.com>
2020-05-04 01:48:17 +0100

    finishing up

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,32 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { Email } from '../email';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
   styleUrls: ['./email-reply.component.css']
 })
-export class EmailReplyComponent implements OnInit {
+export class EmailReplyComponent implements OnChanges {
 
   showModal = false;
   @Input() email: Email;
 
-  constructor() { }
+  constructor(private emailService: EmailService) { }
 
 
-  ngOnInit() {
+  ngOnChanges() {
     const text = this.email.text.replace(/\n/gi, '\n> '); // replace every new line in text with >
     this.email = {  // WOW! Really neat way to swap fields!
       ...this.email,
       from: this.email.to,
       to: this.email.from,
-      subject: 'RE' + this.email.subject,
+      subject: `RE: ${this.email.subject}`,
       text: `\n\n\n-------${this.email.from} wrote:\n>${text}`
     };
   }
 
   onSubmit(email: Email) {
-
+    this.emailService.sendEmail(email).subscribe( () => {
+      this.showModal = false;
+    });
   }
 
 }
